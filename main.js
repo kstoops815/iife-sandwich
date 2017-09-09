2b30f37fd99585ba502878a3d48889c5170241b0
Kelly Stoops <kstoops815@gmail.com>
2017-09-09 09:01:35 -0500

    working on populating the checkboxs

--- main.js
+++ main.js
@@ -6,11 +6,36 @@ console.log("myBread", myBread);
 let myCheese = Sandwich.getCheese();
 console.log("myCheese", myCheese);
 
-let myCondiments = Sandwich.getCondiments();
-console.log("myCondiments", myCondiments);
+// let myCondiments = Sandwich.getCondiments();
+// console.log("myCondiments", myCondiments);
 
-let myMeat = Sandwich.getMeat();
-console.log("myMeat", myMeat);
+// let myMeat = Sandwich.getMeat();
+// console.log("myMeat", myMeat);
 
-let myVeggies = Sandwich.getVeggies();
-console.log("myVeggies", myVeggies);
+// let myVeggies = Sandwich.getVeggies();
+// console.log("myVeggies", myVeggies);
+
+// let breadName = Object.keys(myBread);
+// console.log("breadName", breadName);
+
+
+const buildDomString = (selections) => {
+	let name = Object.keys(selections);
+	console.log(name);
+	let price = Object.values(selections);
+	console.log(price);
+	let finalString = "";
+ 	for(var i = 0; i < name.length; i++){
+		let domString = "";
+		domString +=	`<input type="checkbox" name="${name[i]}" value="${price[i]}">${name[i]}<br>`;
+		finalString += domString;
+	}
+	writeToDom(finalString);
+}
+
+const writeToDom = (items) => {
+	document.getElementById("menuItems").innerHTML += items;
+}
+
+buildDomString(myBread);
+buildDomString(myCheese);
